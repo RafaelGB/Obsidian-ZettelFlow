459e178b9245074da79280eab904963857c43bfe
Rafael Gómez Bermejo <sernn2@gmail.com>
2023-09-01 09:26:18 +0200

    label added to modal

--- src/architecture/lang/locale/en.ts
+++ src/architecture/lang/locale/en.ts
@@ -21,6 +21,8 @@ export default {
     step_builder_root_toggle_description: 'This toggle will set the current element as the root element.',
     step_builder_step_title: 'Step title',
     step_builder_step_title_description: 'This is the title of the step note.',
+    step_builder_section_label_title: 'Section label',
+    step_builder_section_label_description: 'This is the label of the section that will be displayed as option in the menu.',
     step_builder_element_type_selector_title: 'Element type selector',
     step_builder_element_type_selector_description: 'Choose the type of action you want to perform.',
     step_builder_target_folder_title: 'Target folder',

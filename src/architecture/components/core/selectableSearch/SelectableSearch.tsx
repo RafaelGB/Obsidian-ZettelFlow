6d3bb4f129f28c41f052bc53684d7ac4bd0f94b4
Rafael Gómez Bermejo <sernn2@gmail.com>
2023-11-03 17:20:00 +0100

    list displayed correctly

--- src/architecture/components/core/selectableSearch/SelectableSearch.tsx
+++ src/architecture/components/core/selectableSearch/SelectableSearch.tsx
@@ -1,16 +1,23 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useRef, useState } from "react";
 import { SelectableSearchType } from "./typing";
 import { c } from "architecture/styles/helper";
+import { Icon } from "architecture/components/icon";
 
 export function SelectableSearch(props: SelectableSearchType) {
-  const { options, initialSelections, onChange } = props;
-
-  const [optionsState, setOptionsState] = useState(options);
+  const { options, initialSelections, onChange, placeholder } = props;
+  const [filteredOptions, setFilteredOptions] = useState(options);
+  /* Current selected options*/
   const [initialSelectionsState, setInitialSelectionsState] = useState(
     initialSelections || []
   );
   const [visibleOptions, setVisibleOptions] = useState<boolean>(false);
-  /* Search */
+
+  /* Search list */
+  const listRef = useRef<HTMLUListElement>(null);
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
+
+  /* Search input */
+  const inputRef = useRef<HTMLInputElement>(null);
   const [searchState, setSearchState] = useState("");
   const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
@@ -25,35 +32,98 @@ export function SelectableSearch(props: SelectableSearchType) {
 
   return (
     <div className={c("selectable-search")}>
-      <div className={c("selectable-pill-group")}>
+      <div
+        className={c("selectable-pill-group")}
+        onClick={() => {
+          inputRef.current?.focus();
+        }}
+      >
         {initialSelectionsState.map((option, index) => (
           <div key={`${option}-${index}`} className={c("selectable-pill")}>
             <label onClick={() => {}}>{option}</label>
+            <button
+              onClick={(event) => {
+                event.stopPropagation();
+                // Remove from selected options
+                const newSelectedOptions = initialSelectionsState.filter(
+                  (selectedOption) => selectedOption !== option
+                );
+                setInitialSelectionsState(newSelectedOptions);
+                setFilteredOptions([...filteredOptions, option]);
+                onChange(newSelectedOptions);
+              }}
+            />
           </div>
         ))}
       </div>
       <input
-        type="text"
+        type="search"
+        ref={inputRef}
         value={searchState}
         onChange={handleSearchChange}
-        onFocus={() => setVisibleOptions(true)}
-        onBlur={() => setVisibleOptions(false)}
-        placeholder="Buscar..."
+        onKeyDown={(e) => {
+          switch (e.key) {
+            case "ArrowUp": {
+              e.preventDefault();
+              setSelectedIndex((prevIndex) => Math.max(0, prevIndex - 1));
+              break;
+            }
+            case "ArrowDown": {
+              e.preventDefault();
+              setSelectedIndex((prevIndex) =>
+                Math.min(Object.keys(filteredOptions).length - 1, prevIndex + 1)
+              );
+              break;
+            }
+            case "Enter": {
+              e.preventDefault();
+              const valueFromCurrentIndex = filteredOptions[selectedIndex];
+              const newSelectedOptions = [
+                ...initialSelectionsState,
+                valueFromCurrentIndex,
+              ];
+
+              setInitialSelectionsState(newSelectedOptions);
+              setFilteredOptions(
+                filteredOptions.filter(
+                  (option) => option !== valueFromCurrentIndex
+                )
+              );
+              setVisibleOptions(false);
+              break;
+            }
+          }
+        }}
+        placeholder={placeholder}
+        onFocus={() => {
+          setVisibleOptions(true);
+          setSelectedIndex(0);
+        }}
       />
-      <button onClick={handleClearAll}>Borrar Todo</button>
+      <button onClick={handleClearAll}>
+        <Icon name="cross" />
+      </button>
       {visibleOptions && (
-        <div className={c("selectable-search-options")}>
-          {optionsState
-            .filter(
-              (option) =>
-                !searchState.toLowerCase().includes(option.toLowerCase())
-            )
-            .map((option, index) => (
-              <div key={`${option}-${index}`} className={c("search-pill")}>
-                <label onClick={() => {}}>{option}</label>
-              </div>
-            ))}
-        </div>
+        <ul className={c("search-results")} ref={listRef}>
+          {filteredOptions.map((option, index) => (
+            <li
+              key={`${option}-${index}`}
+              className={
+                index === selectedIndex
+                  ? c("search-selected")
+                  : c("search-hidden")
+              }
+              onClick={() => {
+                const newSelectedOptions = [...initialSelectionsState, option];
+                setInitialSelectionsState(newSelectedOptions);
+                onChange(newSelectedOptions);
+                setSearchState("");
+              }}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
       )}
     </div>
   );
--- src/architecture/components/core/selectableSearch/typing.ts
+++ src/architecture/components/core/selectableSearch/typing.ts
@@ -2,4 +2,5 @@ export type SelectableSearchType = {
     options: string[],
     onChange: (selections: string[]) => void
     initialSelections?: string[],
+    placeholder?: string,
 }

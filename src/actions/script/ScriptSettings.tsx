732eb86b26c2f1d609539de43564b80ffbf8cf0f
Rafael Gómez Bermejo <sernn2@gmail.com>
2023-11-25 15:06:57 +0100

    stable versions

--- src/actions/script/ScriptSettings.tsx
+++ src/actions/script/ScriptSettings.tsx
@@ -3,7 +3,6 @@ import { t } from "architecture/lang";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { CodeEditor } from "./components/CodeEditor";
-import CodeMirror from "codemirror";
 
 export const scriptSettings: ActionSetting = (contentEl, _, action) => {
   contentEl.createEl("h3", {
@@ -12,7 +11,6 @@ export const scriptSettings: ActionSetting = (contentEl, _, action) => {
   contentEl.createEl("p", {
     text: t("step_builder_element_type_script_description"),
   });
-  CodeMirror;
   const root = createRoot(contentEl.createDiv());
   root.render(<CodeEditor action={action} root={root} />);
 };

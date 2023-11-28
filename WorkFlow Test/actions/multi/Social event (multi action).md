056cefa28b555a254d9968c829e34030059fd0a9
Rafael Gómez Bermejo <sernn2@gmail.com>
2023-11-28 19:51:46 +0100

    tooltip

--- WorkFlow Test/actions/multi/Social event (multi action).md
+++ WorkFlow Test/actions/multi/Social event (multi action).md
@@ -13,12 +13,11 @@ zettelFlowSettings:
       label: Date of the event
       hasUI: true
       description: Event Date
+    - type: script
+      hasUI: false
+      description: pruebas
+      code: console.log("hello");
   label: Social event (multi action)
   childrenHeader: ""
   targetFolder: zettelFlow/fleeting/events
 ---
-```js
-function aa(){
-
-}
-```
--- src/actions/script/config/EditorConfig.ts
+++ src/actions/script/config/EditorConfig.ts
@@ -1,6 +1,6 @@
 import { basicSetup } from "codemirror";
 import { EditorState } from "@codemirror/state";
-import { EditorView, ViewUpdate, placeholder, lineNumbers } from "@codemirror/view";
+import { EditorView, ViewUpdate, placeholder, lineNumbers, tooltips } from "@codemirror/view";
 import { javascript } from "@codemirror/lang-javascript";
 import { autocompletion } from "@codemirror/autocomplete";
 import { codeFolding, bracketMatching } from "@codemirror/language";
@@ -18,6 +18,7 @@ export function dispatchEditor(parentEl: HTMLDivElement, code: string, onChange:
                 codeFolding(),
                 bracketMatching(),
                 lineNumbers(),
+                tooltips(),
                 javascript(),
                 placeholder("// Enter code here..."),
                 // Listener to update the 'code' variable when the editor changes
